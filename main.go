dfe06d9b53f3d0ff0f42d45d88540a0e78ee76bb
Omar H <contact@omarh.net>
2017-04-13 23:03:38 +0200

    Make view handler

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Data struct {
+	ID string
+}
+
 func homeHandler(t *template.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		err := t.ExecuteTemplate(w, "index.html", nil)
@@ -19,20 +23,31 @@ func homeHandler(t *template.Template) http.HandlerFunc {
 
 func viewHandler(t *template.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
 		id := ""
-		err := t.ExecuteTemplate(w, "index.html", id)
+		if vars["id"] != "" {
+			id = vars["id"]
+		}
+		err := t.ExecuteTemplate(w, "view.html", &Data{ID: id})
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+			log.Fatal(err)
 		}
 	}
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func main() {
 	r := mux.NewRouter()
 	indexTemplate := template.Must(template.ParseFiles("templates/index.html"))
+	viewTemplate := template.Must(template.ParseFiles("templates/view.html"))
 
 	r.HandleFunc("/", homeHandler(indexTemplate)).Methods("GET")
-	r.HandleFunc("/{id}/", viewHandler)
+	r.HandleFunc("/{id}/", viewHandler(viewTemplate)).Methods("GET")
+	r.HandleFunc("/upload/", uploadHandler).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
