69ff0ca79d0e42c7447f87ff62ea0a33bf96b56f
Omar H <contact@omarh.net>
2017-04-22 00:17:03 +0200

    Create util package and cleanup

--- main.go
+++ main.go
@@ -7,23 +7,13 @@ import (
 	"net/http"
 	"net/textproto"
 	"os"
-	"strings"
-	"path/filepath"
-	"math/rand"
-	"time"
 	"io/ioutil"
 	"encoding/json"
 	"strconv"
 
+	"github.com/Omar-H/goimage/util"
 	"github.com/gorilla/csrf"
 	"github.com/gorilla/mux"
-)
-
-const (
-	letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-	letterIdxBits = 6
-	letterIdxMask = 1<<letterIdxBits - 1
-	letterIdxMax  = 63 / letterIdxBits
 )
 
 type ViewData struct {
@@ -34,9 +24,40 @@ type Config struct {
 	Port int `json:"port"`
 	Secure bool `json:"secure"`
 	AuthKey string `json:"32-byte-auth-key"`
+	AllowedMimeTypes []string `json:"allowed-mime-types"`
+	AllowedExtensions []string `json:"allowed-extensions"`
+	MaxFileSize int64 `json:"max-file-size"`
+	ImageDirectory string `json:"image-directory"`
+	TemplateDirectory string `json:"template-directory"`
 }
 
-func homeHandler(t *template.Template) http.HandlerFunc {
+var config Config
+
+func main() {
+	jsonFiles, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	json.Unmarshal(jsonFiles, &config)
+
+	CSRF := csrf.Protect(
+		[]byte(config.AuthKey),
+		csrf.RequestHeader("X-CSRF-Token"),
+		csrf.FieldName("_csrf"),
+		csrf.Secure(config.Secure),
+	)
+	r := mux.NewRouter()
+	templates := template.Must(template.ParseGlob(config.TemplateDirectory + "*.html"))
+
+	r.HandleFunc("/", HomeHandler(templates)).Methods("GET")
+	r.HandleFunc("/{id}/", ViewHandler(templates)).Methods("GET")
+	r.HandleFunc("/upload/", UploadHandler).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":" + strconv.Itoa(config.Port), CSRF(r)))
+}
+
+func HomeHandler(t *template.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-CSRF-Token", csrf.Token(r))
 		err := t.ExecuteTemplate(w, "index.html", map[string]interface{}{
@@ -48,7 +69,7 @@ func homeHandler(t *template.Template) http.HandlerFunc {
 	}
 }
 
-func viewHandler(t *template.Template) http.HandlerFunc {
+func ViewHandler(t *template.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		id := ""
@@ -66,8 +87,8 @@ func viewHandler(t *template.Template) http.HandlerFunc {
 	}
 }
 
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(30000000)
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(config.MaxFileSize)
 	file, handler, err := r.FormFile("file")
 	if err != nil {
 		log.Fatal(err)
@@ -75,13 +96,13 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Close()
 
-	if checkFileType(handler.Header) == false {
+	if CheckFileType(handler.Header) == false {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
 
-	name := generateName(5)
-	f, err := os.OpenFile("./images/" + name + getFileExt(handler.Header["Content-Disposition"][0]), os.O_WRONLY|os.O_CREATE, 0666)
+	name := GenerateName(5)
+	f, err := os.OpenFile(config.ImageDirectory + name + util.GetFileExt(handler.Header["Content-Disposition"][0]), os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -92,102 +113,24 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/" + name + "/", http.StatusSeeOther)
 }
 
-func checkFileType(f textproto.MIMEHeader) bool {
-	allowed := []string{
-		"image/x-icon",
-		"image/jpeg",
-		"image/pjpeg",
-		"image/png",
-		"image/tiff",
-		"image/x-tiff",
-		"image/webp",
-		"image/gif",
-	}
 
-	allowedExt := []string{
-		".png",
-		".jpeg",
-		".jpg",
-		".jiff",
-		".png",
-		".ico",
-		".gif",
-		".tif",
-		".webp",
-	}
 
-	ext := getFileExt(f["Content-Disposition"][0])
+func CheckFileType(f textproto.MIMEHeader) bool {
+	ext := util.GetFileExt(f["Content-Disposition"][0])
 
-	if !contains(allowed, f["Content-Type"][0]) || !contains(allowedExt, ext) {
+	if !util.Contains(config.AllowedMimeTypes, f["Content-Type"][0]) || !util.Contains(config.AllowedExtensions, ext) {
 		return false
 	}
 
 	return true
 }
 
-func getFileExt(s string) string {
-	Ext := strings.Split(s, ";")
-	var ext string
+func GenerateName(n int) string {
+	name := util.GenerateName(n)
 
-	for _, e := range Ext {
-		if strings.Contains(e, "filename") {
-			ext = filepath.Ext(strings.Trim(strings.Split(e, "=\"")[1], "\""))
-		}
+	for util.CheckExists(name, "images/") {
+		name = util.GenerateName(n)
 	}
 
-	return ext
-}
-
-func generateName(n int) string {
-	var src = rand.NewSource(time.Now().UnixNano())
-
-	b := make([]byte, n)
-	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
-		if remain == 0 {
-			cache, remain = src.Int63(), letterIdxMax
-		}
-		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-			b[i] = letterBytes[idx]
-			i--
-		}
-		cache >>= letterIdxBits
-		remain--
-	}
-
-	return string(b)
-}
-
-func contains(s []string, e string) bool {
-	for _, a := range s {
-		if a == e {
-			return true
-		}
-	}
-	return false
-}
-
-func main() {
-	jsonFiles, err := ioutil.ReadFile("./config.json")
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	var config Config
-	json.Unmarshal(jsonFiles, &config)
-
-	CSRF := csrf.Protect(
-		[]byte(config.AuthKey),
-		csrf.RequestHeader("X-CSRF-Token"),
-		csrf.FieldName("_csrf"),
-		csrf.Secure(config.Secure),
-	)
-	r := mux.NewRouter()
-	templates := template.Must(template.ParseGlob("templates/*.html"))
-
-	r.HandleFunc("/", homeHandler(templates)).Methods("GET")
-	r.HandleFunc("/{id}/", viewHandler(templates)).Methods("GET")
-	r.HandleFunc("/upload/", uploadHandler).Methods("POST")
-
-	log.Fatal(http.ListenAndServe(":" + strconv.Itoa(config.Port), CSRF(r)))
+	return name
 }
---
+++ util/util.go
@@ -0,0 +1,69 @@
+package util
+
+import (
+	"io/ioutil"
+	"strings"
+	"time"
+	"path/filepath"
+	"math/rand"
+)
+
+const (
+	letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+	letterIdxBits = 6
+	letterIdxMask = 1 << letterIdxBits - 1
+	letterIdxMax  = 63 / letterIdxBits
+)
+
+
+
+func Contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+func CheckExists(name string, dir string) bool {
+	files, _ := ioutil.ReadDir(dir)
+	for _, f := range files {
+		if strings.Contains(f.Name(), name) {
+			return true
+		}
+	}
+	return false
+}
+
+func GenerateName(n int) string {
+	var src = rand.NewSource(time.Now().UnixNano())
+
+	b := make([]byte, n)
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
+
+func GetFileExt(s string) string {
+	Ext := strings.Split(s, ";")
+	var ext string
+
+	for _, e := range Ext {
+		if strings.Contains(e, "filename") {
+			ext = filepath.Ext(strings.Trim(strings.Split(e, "=\"")[1], "\""))
+		}
+	}
+
+	return ext
+}
