d0776144786b097e82ab980e1f95865a0f50608e
Omar A. Hachach <ohachach@gmail.com>
2018-11-02 00:14:39 +0100

    Create all API errors.

---
+++ api/error.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"net/http"
+)
+
+// ErrorResponse is the API error response format.
+type ErrorResponse struct {
+	Code  int    `json:"code,omitempty"`
+	Error string `json:"error,omitempty"`
+}
+
+// This holds the default errors.
+var (
+	ErrInternal     = NewError(http.StatusInternalServerError, "An internal server error has occured.")
+	ErrBadRequest   = NewError(http.StatusBadRequest, "Request could not be processed. Bad request.")
+	ErrMissingFile  = NewError(http.StatusBadRequest, "The file is missing.")
+	ErrFileTooLarge = NewError(http.StatusRequestEntityTooLarge, "The file is too large.")
+	ErrFileType     = NewError(http.StatusUnprocessableEntity, "The file type is not supported.")
+)
+
+// NewError returns a new ErrorResponse
+func NewError(code int, err string) ErrorResponse {
+	return ErrorResponse{
+		Code:  code,
+		Error: err,
+	}
+}
